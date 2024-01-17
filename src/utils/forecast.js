be1fed082c5948c0ecd47c20c4581c72a728830c
Hava Hoxha <60502737+hoxhaava@users.noreply.github.com>
2024-01-17 20:58:16 +0100

    Update forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request = require('postman-request')
 
 const forecast = (lat, long, callback) => {
-    const url = 'http://api.weatherstack.com/current?access_key=276fea229a48d1e5cd804415f6486570&query=' + encodeURIComponent(lat) + ', ' + encodeURIComponent(long)
+    const url = 'process.env.WEATHER_STACK_URL + encodeURIComponent(lat) + ', ' + encodeURIComponent(long)
     request({ url, json: true }, (error, {body} ) => {
         if (error) {
             callback('Unable to connect to weather services.', undefined)
